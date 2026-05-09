fff370cb4f3e55e43587ef84ec834a6aaeee3184
fjsv09 <fjsv.cjoseph@gmail.com>
2026-05-09 08:47:58 -0500

    fix: refinamiento de animaciones y consistencia visual en diálogos y alertas

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -30,15 +30,16 @@ AlertDialogOverlay.displayName = AlertDialogPrimitive.Overlay.displayName
 const AlertDialogContent = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>
->(({ className, ...props }, ref) => (
+>(({ className, style, ...props }, ref) => (
   <AlertDialogPortal>
     <AlertDialogOverlay />
     <AlertDialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-[500] grid w-[94vw] sm:w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-2xl sm:rounded-lg max-h-[96dvh] overflow-y-auto custom-scrollbar",
+        "fixed left-[50%] top-[50%] z-[500] grid w-[94vw] sm:w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-2xl sm:rounded-lg overflow-y-auto custom-scrollbar",
         className
       )}
+      style={{ maxHeight: 'min(85dvh, calc(100dvh - 2 * env(safe-area-inset-top, 0px)))', ...style }}
       {...props}
     />
   </AlertDialogPortal>
